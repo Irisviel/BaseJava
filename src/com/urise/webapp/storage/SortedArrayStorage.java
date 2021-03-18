6ef99b45531c7687b21c741782a9bb4127e5e0a3
Ivanov Konstantin <konst2iva@gmail.com>
2021-03-18 11:12:09 +0400

    Removed inherited methods

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -5,36 +5,6 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
-    public void clear() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void update(Resume resume) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void save(Resume resume) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Resume get(String uuid) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public void delete(String uuid) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Resume[] getAll() {
-        throw new NotImplementedException();
-    }
-
-    @Override
     protected int getIndex(String uuid) {
         throw new NotImplementedException();
     }
@@ -46,11 +16,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void fillDeletedElement(int index) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public int size() {
         throw new NotImplementedException();
     }
 }
