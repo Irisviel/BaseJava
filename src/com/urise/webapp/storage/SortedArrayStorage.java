dc651836fbbcd99a37af4d3da40e46436adf6863
Ivanov Konstantin <konst2iva@gmail.com>
2021-03-19 17:19:33 +0400

    06 Specified comparator for Resume

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,11 +3,15 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
+
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid, "stub");
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     protected void insertElement(Resume resume, int index) {
