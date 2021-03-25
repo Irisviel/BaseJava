412ddc44f88dceba0bfeb997fe85af4e6006dc86
Ivanov Konstantin <konst2iva@gmail.com>
2021-03-25 19:44:31 +0400

    06 lesson05 Removed unnecessary stub

--- test/com/urise/webapp/storage/MapUuidStorageTest.java
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -1,19 +1,8 @@
 package com.urise.webapp.storage;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
-
 public class MapUuidStorageTest extends AbstractStorageTest {
 
     public MapUuidStorageTest() {
         super(new MapUuidStorage());
-    }
-
-    @Override
-    @Test
-    // TODO: implement
-    public void getAllSorted() {
-        assertTrue(true);
     }
 }
