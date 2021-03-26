41cd48f9017f1df802bad2a66eb6ef838b24ee53
Ivanov Konstantin <konst2iva@gmail.com>
2021-03-26 17:31:56 +0400

    07 Update model. Added Link. Updated Organization.

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,21 +1,59 @@
 package com.urise.webapp.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Organization {
+    private final Link homePage;
+
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final String title;
+    private final String description;
+
+    public Organization(String name, String url, LocalDate startDate, LocalDate endDate, String title, String description) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(endDate, "endDate must not be null");
+        Objects.requireNonNull(title, "title must not be null");
+        this.homePage = new Link(name, url);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.description = description;
+    }
+
     @Override
     public int hashCode() {
-        return super.hashCode();
+        int result = homePage.hashCode();
+        result = 31 * result + startDate.hashCode();
+        result = 31 * result + endDate.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Organization that = (Organization) obj;
+
+        if (!homePage.equals(that.homePage)) return false;
+        if (!startDate.equals(that.startDate)) return false;
+        if (!endDate.equals(that.endDate)) return false;
+        if (!title.equals(that.title)) return false;
+        return Objects.equals(description, that.description);
     }
 
     @Override
     public String toString() {
-        return super.toString();
-    }
-
-    public Organization() {
+        return "Organization{" +
+                "homePage=" + homePage +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
