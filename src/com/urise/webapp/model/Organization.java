b21191b28bc4e988f55aa01067f837ffb4f56c38
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-01 21:55:16 +0400

    08 Added DateUtil. Made endDate in Position optional.

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,9 +1,12 @@
 package com.urise.webapp.model;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+import static com.urise.webapp.util.DateUtil.of;
 
 public class Organization {
     private final Link homePage;
@@ -44,12 +47,19 @@ public class Organization {
 
         public Position(LocalDate startDate, LocalDate endDate, String title, String description) {
             Objects.requireNonNull(startDate, "startDate must not be null");
-            Objects.requireNonNull(endDate, "endDate must not be null");
             Objects.requireNonNull(title, "title must not be null");
             this.startDate = startDate;
             this.endDate = endDate;
             this.title = title;
             this.description = description;
+        }
+
+        public Position(int startYear, Month startMonth, String title, String description) {
+            this(of(startYear, startMonth), null, title, description);
+        }
+
+        public Position(int startYear, Month startMonth, int endYear, Month endMonth, String title, String description) {
+            this(of(startYear, startMonth), of(endYear, endMonth), title, description);
         }
 
         public LocalDate getStartDate() {
