fb18b3c2a53862003e25b6e872b0d85318bb2ebc
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-02 01:03:39 +0400

    08 Added AbstractFileStorage

---
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,103 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    protected abstract void doWrite(Resume resume, File file) throws IOException;
+
+    protected abstract Resume doRead(File file) throws IOException;
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, File file) {
+        try {
+            doWrite(resume, file);
+        } catch (IOException exception) {
+            throw new StorageException("Unable to write file", resume.getUuid(), exception);
+        }
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(Resume resume, File file) {
+        try {
+            file.createNewFile();
+        } catch (IOException exception) {
+            throw new StorageException("Unable to create file " + file.getName(), file.getName(), exception);
+        }
+        doUpdate(resume, file);
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(file);
+        } catch (IOException exception) {
+            throw new StorageException("Unable to read file " + file.getName(), file.getName(), exception);
+        }
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new StorageException("Unable to delete file" + file.getName(), file.getName());
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            throw new StorageException("Invalid path to storage directory", null);
+        }
+        return Arrays.stream(files).map(this::doGet).collect(Collectors.toList());
+    }
+
+    @Override
+    public void clear() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                doDelete(file);
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        String[] list = directory.list();
+        if (list == null) {
+            throw new StorageException("Invalid path to storage directory", null);
+        }
+        return list.length;
+    }
+}
