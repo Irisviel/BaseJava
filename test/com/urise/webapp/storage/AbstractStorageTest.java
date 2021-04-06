6aa9a2a97c28c76f9db92f19b5ad9d8465b27227
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-07 03:59:52 +0400

    09 Added ObjectStreamFileStorage, ObjectStreamPathStorage, + tests

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import com.urise.webapp.model.Resume;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +14,8 @@ import static com.urise.webapp.ResumeTestData.newResume;
 import static org.junit.Assert.assertEquals;
 
 public abstract class AbstractStorageTest {
+    protected static final File STORAGE_DIR = new File("C:\\projects\\storage");
+
     protected Storage storage;
 
     private static final String UUID_1 = "uuid1";
