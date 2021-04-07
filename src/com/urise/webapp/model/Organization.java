4c181cdae8674a77c596b19c581de97568dd198b
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-07 04:12:27 +0400

    09 Marked model classes as Serializable

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -7,7 +8,9 @@ import java.util.Objects;
 
 import static com.urise.webapp.util.DateUtil.of;
 
-public class Organization {
+public class Organization implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final Link homePage;
     private List<Position> positions;
 
@@ -46,7 +49,9 @@ public class Organization {
                 '}';
     }
 
-    public static class Position {
+    public static class Position implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         private final LocalDate startDate;
         private final LocalDate endDate;
         private final String title;
