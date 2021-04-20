5529f85dd4946538e493582fbba8fd9da1cf993d
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-21 02:08:33 +0400

    13 Added SqlStorage implementation

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -10,6 +10,7 @@ import com.urise.webapp.model.Resume;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static com.urise.webapp.ResumeTestData.newResume;
 import static org.junit.Assert.assertEquals;
@@ -19,10 +20,10 @@ public abstract class AbstractStorageTest {
 
     protected Storage storage;
 
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid4";
+    private static final String UUID_1 = UUID.randomUUID().toString();
+    private static final String UUID_2 = UUID.randomUUID().toString();
+    private static final String UUID_3 = UUID.randomUUID().toString();
+    private static final String UUID_4 = UUID.randomUUID().toString();
     private static final String DUMMY = "dummy";
 
     private static final Resume RESUME_1;
