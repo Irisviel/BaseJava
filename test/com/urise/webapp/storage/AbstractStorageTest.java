259228caed716cc2612ce8fedfe5b506512c5a03
Ivanov Konstantin <konst2iva@gmail.com>
2021-04-21 00:03:50 +0400

    13 Database init & connection

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import org.junit.Before;
@@ -14,7 +15,7 @@ import static com.urise.webapp.ResumeTestData.newResume;
 import static org.junit.Assert.assertEquals;
 
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR = new File("C:\\projects\\basejava\\storage");
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
 
     protected Storage storage;
 
