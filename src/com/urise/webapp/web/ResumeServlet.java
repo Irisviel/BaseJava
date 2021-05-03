ccf3497e8e9a8fe95aef6d66af131a662c50eb31
Ivanov Konstantin <konst2iva@gmail.com>
2021-05-03 18:42:33 +0400

    15 Added resume table output to servlet

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -29,6 +31,31 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        Writer writer = response.getWriter();
+        writer.write("" +
+                "<!doctype html>\n" +
+                "\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=\"utf-8\">\n" +
+                "  <title>Список всех резюме</title>\n" +
+                "  <meta name=\"description\" content=\"Список всех резюме\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n"
+        );
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write("" +
+                    "<tr>\n" +
+                    "<td>" + resume.getUuid() + "</td>\n" +
+                    "<td>" + resume.getFullName() + "</td>\n" +
+                    "</tr>\n"
+            );
+        }
+        writer.write("" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>"
+        );
     }
 }
