3b161d00487c334bbd9eb97f5169d20b48d4d8ba
Ivanov Konstantin <konst2iva@gmail.com>
2021-05-03 15:14:54 +0400

    15 Added section table to init_db

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,13 +1,22 @@
-CREATE TABLE resume (
+CREATE TABLE IF NOT EXISTS resume (
   uuid      CHAR(36) PRIMARY KEY NOT NULL,
   full_name TEXT                 NOT NULL
 );
 
-CREATE TABLE contact (
+CREATE TABLE IF NOT EXISTS contact (
   id          SERIAL,
   resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
   type        TEXT     NOT NULL,
   value       TEXT     NOT NULL
 );
-CREATE UNIQUE INDEX contact_uuid_type_index
+CREATE UNIQUE INDEX IF NOT EXISTS contact_uuid_type_index
   ON contact (resume_uuid, type);
+
+CREATE TABLE IF NOT EXISTS section (
+  id          SERIAL PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS section_idx
+  ON section (resume_uuid, type);
